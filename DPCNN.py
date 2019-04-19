5cd175b97797d06cb3c363dcfc4dabb3edf4ec91
Evan <yiran_zhou@brown.edu>
2019-04-19 14:41:12 -0400

    init project

---
+++ DPCNN.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import utils
+import keras
+import numpy
+
+class DPCNN():
+    def __init__(self,inputs,labels,output_size,learning_rate,drop_out,hidden_layers,epochs,batchSz):
+        pass
+    def construct(self):
+        pass
+    def train(self,verbose=0):
+        pass
+    def predict(self,inputs,verbose=0):
+        pass
+    def test(self,test_inputs,test_labels,verbose=0):
+        pass
