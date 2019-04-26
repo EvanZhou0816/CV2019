b5ec5424dc3e8e8e2d77b76dfd34e4de23c041fc
Jia <julia@Jias-MacBook-Pro.local>
2019-04-25 22:10:23 -0400

    Done: single filename -> h*w*3 /255 np array

--- raw_data.py
+++ raw_data.py
@@ -28,13 +28,17 @@ from PIL import Image
 def process_image(filename):
     img = Image.open(filename)
     if img.width > 600 or img.height > 600:
-        if 1.0*img.width/600 > 1.0*img.height/600:
-            scale = 1.0*img.width/600
+        w_ratio = img.width/600
+        h_ratio = img.height/600
+        if w_ratio > h_ratio:
+            scale = w_ratio
         else:
-            scale = 1.0*img.height/600
+            scale = h_ratio
+        # https://pillow.readthedocs.io/en/3.0.x/releasenotes/2.7.0.html
+        # https://zhuanlan.zhihu.com/p/27504020
         img = img.resize((int(img.width/scale), int(img.height/scale)), Image.ANTIALIAS)
-    img.save('new-'+filename)
-    img.close()
+    # img.save('new-'+filename)
+    # img.close()
     arr = np.array(img)
     res = arr/255
     return res
