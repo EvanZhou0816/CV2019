b7a979e9fe145aea7d92408e3d0f246dfa2a9e96
Jia <julia@jias-mbp.devices.brown.edu>
2019-04-27 14:47:45 -0400

    Done: process_image

--- raw_data.py
+++ raw_data.py
@@ -1,29 +1,6 @@
-import gzip
 import numpy as np
-import tarfile
 
 #image total 5062
-# def raw_data():
-#     i = 0
-#     tar = tarfile.open("oxbuild_images.tgz")
-#     for member in tar.getmembers():
-#         file = tar.extractfile(member)
-#         temp = np.fromstring(file.read(), dtype=np.uint8)
-#         temp /= 255
-#         if not i % 250:
-#             print("%d images to array" % i)
-#     print("All images to array")
-# raw_data()
-
-
-# from scipy import misc
-# rgb = np.zeros((255, 255, 3), dtype=np.uint8)
-# rgb[..., 0] = np.arange(255)
-# rgb[..., 1] = 55
-# rgb[..., 2] = 1 - np.arange(255)
-# misc.imsave('/tmp/rgb_gradient.png', rgb)
-# print(rgb)
-
 from PIL import Image
 def process_image(filename):
     img = Image.open(filename)
